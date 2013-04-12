74591a0542200029ab00675c4e9e318a37471d87
Gabor Szarnyas <szarnyasg@gmail.com>
2013-04-12 20:29:57 +0200

    simple & fast working version

--- neo4j.graphml.indexing/src/main/java/neo4j/storage/main/Neo4jLoader.java
+++ neo4j.graphml.indexing/src/main/java/neo4j/storage/main/Neo4jLoader.java
@@ -24,95 +24,30 @@ import com.tinkerpop.blueprints.util.io.graphml.GraphMLReader;
 public class Neo4jLoader {
 	String dbPath = "neo4j-graph";
 	String xmlFile = "graph-of-the-gods.xml";
-
+	
 	public void work() throws IOException {
 		FileUtils.deleteRecursively(new File(dbPath));
-		initializeDb();
-		loadGraphML();
-		retrieve();
-	}
 
-	private void initializeDb() {
-		System.out.println("Initializing graph database.");
 		GraphDatabaseService graphDb = new GraphDatabaseFactory()
 				.newEmbeddedDatabaseBuilder(dbPath)
 				.setConfig(GraphDatabaseSettings.node_keys_indexable, "type")
 				.setConfig(GraphDatabaseSettings.node_auto_indexing, "true")
 				.newGraphDatabase();
-		Transaction tx = graphDb.beginTx();
 
-		Node node = graphDb.createNode();
-		node.setProperty("type", "god");
-		node.setProperty("name", "sample_god");
-		tx.success();
-		tx.finish();
-
-		getGodsByIndex(graphDb);
-		graphDb.shutdown();
-		System.out.println();
-	}
-
-	private void getGodsByIndex(GraphDatabaseService graphDb) {
-		System.out.println("Listing gods:");
-		ReadableIndex<Node> autoNodeIndex = graphDb.index()
-				.getNodeAutoIndexer().getAutoIndex();
-		IndexHits<Node> indexHits = autoNodeIndex.get("type", "god");
-		for (Node node : indexHits) {
+		Neo4jGraph graph = new Neo4jGraph(graphDb);
+		GraphMLReader.inputGraph(graph, new FileInputStream(xmlFile));
+		
+		IndexHits<Node> godsByIndex = getGodsByIndex(graphDb, "god");
+		for (Node node : godsByIndex) {
 			System.out.println(node.getProperty("name"));
 		}
 	}
 
-	private void loadGraphML() throws FileNotFoundException, IOException {
-		System.out.println("Loading the GraphML.");
-		Neo4jGraph graph = new Neo4jGraph(dbPath);
-		GraphMLReader.inputGraph(graph, new FileInputStream(xmlFile));
-		graph.shutdown();
-		System.out.println("GraphML loaded.");
-		System.out.println();
-	}
-
-	private void retrieve() {
-		System.out.println("Retreive nodes.");
-		GraphDatabaseService graphDb = new GraphDatabaseFactory()
-				.newEmbeddedDatabaseBuilder(dbPath)
-				.setConfig(GraphDatabaseSettings.node_keys_indexable, "type")
-				.setConfig(GraphDatabaseSettings.node_auto_indexing, "true")
-				.newGraphDatabase();
-
-		GlobalGraphOperations graphOperations = GlobalGraphOperations
-				.at(graphDb);
-		updateAutoIndex(graphDb, graphDb.index().getNodeAutoIndexer(),
-				graphOperations.getAllNodes());
-
-		getGodsByIndex(graphDb);
-
-		graphDb.shutdown();
-	}
-
-	// source: https://github.com/neo4j/neo4j/issues/173
-	private static <T extends PropertyContainer> void updateAutoIndex(
-			GraphDatabaseService db, AutoIndexer<T> autoIndexer,
-			final Iterable<T> elements) {
-		if (!autoIndexer.isEnabled())
-			return;
-		final Set<String> properties = autoIndexer.getAutoIndexedProperties();
-		Transaction tx = db.beginTx();
-		int count = 0;
-		for (PropertyContainer pc : elements) {
-			for (String property : properties) {
-				if (!pc.hasProperty(property))
-					continue;
-				pc.setProperty(property, pc.getProperty(property));
-				count++;
-				if (count % 10000 == 0) {
-					tx.success();
-					tx.finish();
-					tx = db.beginTx();
-				}
-			}
-		}
-		tx.success();
-		tx.finish();
+	private IndexHits<Node> getGodsByIndex(GraphDatabaseService graphDb, String typeName) {
+		ReadableIndex<Node> autoNodeIndex = graphDb.index()
+				.getNodeAutoIndexer().getAutoIndex();
+		IndexHits<Node> indexHits = autoNodeIndex.get("type", typeName);
+		return indexHits;
 	}
 
 }
